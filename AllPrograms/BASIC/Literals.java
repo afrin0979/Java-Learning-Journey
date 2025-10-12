eb8992124bf790847161de087ae4d2bcc494fc7a
afrin0979 <afrinka04@gmail.com>
2025-10-12 11:35:56 +0530

    Added new programs and updated files in multiple folders

--- AllPrograms/BASIC/Literals.java
+++
@@ -1,9 +0,0 @@
-public class Literals{
-    public static void main(String[]args){
-        double a=565;
-        System.out.println(a);
-
-
-    }
-}
-// litrals in java is actual value  like the value  is a literals 
